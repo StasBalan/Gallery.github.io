fbc0b3df4d0f434330569e8e40c79c61dbff8584
StasBalan <43076181+StasBalan@users.noreply.github.com>
2018-12-09 17:09:58 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ script.src = "https://api.vk.com/method/photos.get?owner_id=-37512548&album_id=1
 document.getElementsByTagName("head")[0].appendChild(script);
 
 function callbackFunc(result){ 
-  for (let i = 0; i < result.response.count; i++){ 
-  let img = document.createElement('img'); 
+  for (var i = 0; i < result.response.count; i++){ 
+  var img = document.createElement('img'); 
   img.src = result.response.items[i].sizes[6].url; 
   img.id = i; 
   img.width = '320';
@@ -14,7 +14,7 @@ function callbackFunc(result){
   img.onclick = function(){
     modal.style.display = "block";
     id = +this.id;
-    let photo = document.createElement('img');
+    var photo = document.createElement('img');
     photo.className = 'modal-content';
     photo.src = result.response.items[id].sizes[6].url;
     document.getElementById('box').appendChild(photo);
@@ -27,15 +27,15 @@ document.getElementById('right').onclick = function(){
     id = -1;
     id++;
   }
-  let modalBox = document.getElementById('box');
-  let main = modalBox.firstChild;
+  var modalBox = document.getElementById('box');
+  var main = modalBox.firstChild;
 
   while (main){
     modalBox.removeChild(main);
     main = modalBox.firstChild;
   }
 
-  let photo = document.createElement('img');
+  var photo = document.createElement('img');
   photo.className = 'modal-content';
   photo.src = result.response.items[id].sizes[6].url;
   document.getElementById('box').appendChild(photo);
@@ -48,34 +48,34 @@ document.getElementById('left').onclick = function() {
     id = +result.response.count - 1;
   }
 
-  let modalBox = document.getElementById('box');
-  let main = modalBox.firstChild;
+  var modalBox = document.getElementById('box');
+  var main = modalBox.firstChild;
   while(main) {
     modalBox.removeChild(main);
     main = modalBox.firstChild;
   }
 
-  let photo = document.createElement('img');
+  var photo = document.createElement('img');
   photo.className = 'modal-content';
   photo.src = result.response.items[id].sizes[6].url;
   document.getElementById('box').appendChild(photo);
 }
 
-  let bottomPhoto = document.createElement('img');
+  var bottomPhoto = document.createElement('img');
   bottomPhoto.src = result.response.items[i].sizes[6].url;
   bottomPhoto.height = '130';
   bottomPhoto.width = '250';
   bottomPhoto.count = 2;
   bottomPhoto.id = i;
   bottomPhoto.position = 0;
-  let li = document.createElement('li');
+  var li = document.createElement('li');
   li.src = result.response.items[i].sizes[6].url;
   li.appendChild(bottomPhoto);
   document.getElementById('modal-carusel__img').appendChild(li);
 
-  let modalCarusel = document.getElementById('modal-carusel');
-  let list = modalCarusel.querySelector('ul');
-  let listElems = modalCarusel.querySelectorAll('li'); 
+  var modalCarusel = document.getElementById('modal-carusel');
+  var list = modalCarusel.querySelector('ul');
+  var listElems = modalCarusel.querySelectorAll('li'); 
 
   document.getElementById('btn_next').onclick = function() {
     bottomPhoto.position = Math.max(bottomPhoto.position - bottomPhoto.width * bottomPhoto.count);
@@ -90,13 +90,13 @@ document.getElementById('left').onclick = function() {
     bottomPhoto.onclick = function() {
       modal.style.display = "block";
     id = +this.id;
-      let modalBox = document.getElementById('box');
-      let main = modalBox.firstChild;
+      var modalBox = document.getElementById('box');
+      var main = modalBox.firstChild;
       while(main) {
       modalBox.removeChild(main);
       main = modalBox.firstChild;
       }
-      let photo = document.createElement('img');
+      var photo = document.createElement('img');
     photo.className = 'modal-content';
     photo.src = result.response.items[id].sizes[6].url;
     document.getElementById('box').appendChild(photo);
@@ -111,7 +111,7 @@ window.onload = function(){
 };
 
 window.onscroll = function() {
-   let scrollElem = document.getElementById("scrollToTop");
+   var scrollElem = document.getElementById("scrollToTop");
    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
       document.getElementById("scrollToTop").style.opacity = "1";
    }
@@ -120,7 +120,7 @@ window.onscroll = function() {
    }
 }
 
-let timeOut;
+var timeOut;
 function goUp() {
   var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
   if(top > 0) {
@@ -131,13 +131,13 @@ function goUp() {
 document.getElementById("scrollToTop").onclick = goUp;
 
 
-let modal = document.getElementById('myModal');
-let closeBtn = document.getElementsByClassName("close")[0];
+var modal = document.getElementById('myModal');
+var closeBtn = document.getElementsByClassName("close")[0];
 
 closeBtn.onclick = function() { 
   modal.style.display = "none";
-  let modalBox = document.getElementById('box');
-  let main = modalBox.firstChild;
+  var modalBox = document.getElementById('box');
+  var main = modalBox.firstChild;
   while(main) {
     modalBox.removeChild(main);
     main = modalBox.firstChild;
